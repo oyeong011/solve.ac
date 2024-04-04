1d655e1a3226c18b431b02503aedee91873f4c26
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-04-04 20:59:39 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,21 +1,17 @@
 from collections import deque
 
-
 n = int(input())
 list = list(map(int, input().split()))
 
-deq = deque(list)
-num = deque([i for i in range(1, n + 1)])
+deq = deque(enumerate(list))
 result = []
 
 for _ in range(len(list)):
-    result.append(num.popleft())
-    move = deq.popleft()
+    n, move = deq.popleft()
+    result.append(n + 1)
     if move > 0:
         deq.rotate(-move + 1)
-        num.rotate(-move + 1)
     else:
         deq.rotate(-move)
-        num.rotate(-move)
 
 print(*result)
