676f351f5897f0cc27cb7ceb65967a0246b31d9a
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-10-02 10:10:49 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,23 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> ABC(4);
-vector<int> CNT(101);
-int in, out;
-int result;
-int main(){
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int ABC[4], CNT[101], in, out, result;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
     cin >> ABC[1] >> ABC[2] >> ABC[3];
 
-    for(int i = 0; i < 3; i++){
+    for (int i = 0; i < 3; i++) {
         cin >> in >> out;
-        for(int j = in; j < out; j++){
+        for (int j = in; j < out; j++) {
             CNT[j]++;
         }
     }
 
-    for(int i = 1; i < 100; i++){
+    for (int i = 1; i < 100;i++) {
         result += ABC[CNT[i]] * CNT[i];
     }
-    
-    cout << result << '\n';
+
+    cout << result << "\n";
 }
