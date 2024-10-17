11e4d04f9e9e35f753131dc5c9f8d09e04c00a28
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-10-17 18:53:39 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,47 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-bool isVowel(char c) {
-    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+int vCnt, comCnt;
+string s;
+bool isVowel(int idx) {
+    return (idx == 'a'|| idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u');
 }
 
 int main() {
+
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
-    string s;
+    cin.tie(NULL);cout.tie(NULL);
     while (cin >> s && s != "end") {
-        int vCnt = 0, comCnt = 0;
+        vCnt = comCnt = 0;
         bool flag = false;
         bool isContain_vo = false;
         char prev = '\0';
 
         for (char it : s) {
-            if (isVowel(it)) {
-                vCnt++; comCnt = 0; isContain_vo = true;
-            } else {
-                comCnt++; vCnt = 0;
-            }
-            
-            if (comCnt == 3 || vCnt == 3) {
-                flag = true;
-                break;
-            }
-            
-            if (prev == it && (it != 'e' && it != 'o')) {
-                flag = true;
-                break;
-            }
-            
+            if (isVowel(it)) {vCnt++; comCnt = 0; isContain_vo = true;}
+            else {comCnt++;vCnt = 0;}
+            if (comCnt == 3 || vCnt == 3){ flag = true; break;}
+            if (prev == it && (it != 'e' && it != 'o')) flag = true;
             prev = it;
         }
+        if (flag || !isContain_vo){ cout << "<" << s << "> is not acceptable.\n";}
+        else  { cout << "<" << s << "> is acceptable.\n"; }
 
-        if (flag || !isContain_vo) {
-            cout << "<" << s << "> is not acceptable.\n";
-        } else {
-            cout << "<" << s << "> is acceptable.\n";
-        }
     }
 
-    return 0;
 }
