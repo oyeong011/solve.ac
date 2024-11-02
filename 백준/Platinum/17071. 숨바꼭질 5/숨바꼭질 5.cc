b24ec770b364fa24365cb4c193e51ea9129483be
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-11-02 17:40:00 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 16 ms, Memory: 6116 KB -BaekjoonHub

---
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxInt = 500000;
+int n, k, visited[2][maxInt + 4], ok, turn = 1;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> n >> k;
+    if(n == k){cout << 0 << "\n"; return 0;}
+    queue<int> q;
+    visited[0][n] = 1;
+    q.push(n);
+    while(q.size()){
+        k += turn;
+        if(k > maxInt)break;
+        if(visited[turn%2][k]){ok = 1; break;}
+        int qSize = q.size();
+        for(int i = 0; i < qSize; i++){
+            int x = q.front(); q.pop();
+            for(int nx : {x - 1, x + 1, 2 * x}){
+                if(nx < 0 || nx > maxInt || visited[turn % 2][nx])continue;
+                visited[turn % 2][nx] = visited[(turn+1) % 2][x] + 1;
+                if(nx == k){ok = 1; break;}
+                q.push(nx);
+            }
+            if(ok)break;
+        }
+        if(ok)break;
+        turn++;
+    }
+    if(ok) cout << turn <<"\n";
+    else cout << -1 << "\n";
+    return 0;
+    
+}
