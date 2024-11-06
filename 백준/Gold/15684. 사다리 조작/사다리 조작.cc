e3899a9075a5f528e9b09fa65203d6ba118bec6b
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-11-06 14:31:35 +0900

    [Gold III] Title: 사다리 조작, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -1,51 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, h, ans = 4;
-bool visited[31][11];
-
-bool check() {
-    for(int i = 1; i <= n; ++i) {
-        int cur = i;
-        for(int j = 1; j <= h; ++j) {
-            if(visited[j][cur]) ++cur;
-            else if(visited[j][cur-1]) --cur;
+int n, m, h, ret = 4, visited[31][11];
+bool check(){
+    for(int i = 1; i <= n; ++i){
+        int start = i;
+        for(int j = 1; j <= h; ++j){
+            if(visited[j][start]) start++;
+            else if(visited[j][start - 1]) start--;
         }
-        if(cur != i) return false;
+        if(start != i)return false;
     }
     return true;
 }
-
-void go(int cnt, int goal) {
-    if(ans < 4) return;  
-    if(cnt == goal) {    
-        if(check()) ans = goal;
+void go(int cnt, int goal){
+    if(ret < 4)return;
+    if(cnt == goal){
+        if(check())ret = goal;
         return;
     }
     
-    for(int j = 1; j < n; ++j) {
-        for(int i = 1; i <= h; ++i) {
-            if(visited[i][j] || visited[i][j+1] || visited[i][j-1]) continue;
+    for(int j = 1; j < n; ++j){
+        for(int i = 1; i <= h; ++i){
+            if(visited[i][j] || visited[i][j+1] || visited[i][j-1])continue;
             visited[i][j] = 1;
             go(cnt + 1, goal);
             visited[i][j] = 0;
-            while(i <= h && !visited[i][j-1] && !visited[i][j+1]) ++i;
+            while(i <= h && !visited[i][j+1] && !visited[i][j-1])++i;
         }
     }
 }
 
-int main() {
-    ios::sync_with_stdio(false); cin.tie(0);
+int main(){
     cin >> n >> m >> h;
-    for(int i = 0; i < m; ++i) {
+    for(int i = 0; i < m; i++){
         int a, b;
         cin >> a >> b;
         visited[a][b] = 1;
     }
     
-    for(int i = 0; i < 4; ++i) {
+    for(int i = 0; i < 4; i++){
         go(0, i);
-        if(ans != 4) {
-            cout << ans << '\n';
+        if(ret != 4){
+            cout << ret << "\n";
             return 0;
         }
     }
