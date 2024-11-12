2464f720640389f04cc5f71e96c903938536e359
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-11-12 15:06:57 +0900

    [Silver V] Title: 집합, Time: 636 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int l = 0, n, num;
+void go(string cmd) {
+    if (cmd == "add") {
+        cin >> num;
+        l |= (1 << num - 1);
+    } else if (cmd == "remove") {
+        cin >> num;
+        l &= ~(1 << num - 1);
+    } else if (cmd == "check") {
+        cin >> num;
+        if (l & (1 << num - 1)) cout << 1 << "\n";
+        else cout << 0 << "\n";
+    } else if (cmd == "toggle") {
+        cin >> num;
+        if (l & (1 << num - 1)) l &= ~(1 << num - 1);
+        else l |= (1 << num - 1);
+    } else if (cmd == "all") {
+        for (int i = 0; i < 20; i++) l |= (1 << i);
+    } else if (cmd == "empty") {
+        l = 0;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string cmd;
+        cin >> cmd;
+        go(cmd);
+    }
+    return 0;
+}
