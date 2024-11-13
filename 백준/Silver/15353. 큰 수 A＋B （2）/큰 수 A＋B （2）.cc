692516f54ec34ad1b8010bc116cdc8d773cf8584
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-11-13 11:15:16 +0900

    [Silver III] Title: 큰 수 A+B (2), Time: 0 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
+++ 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
@@ -1,29 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 string a, b;
-vector<int> v;
-int z;
-int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+string string_add(string a, string b){
+    int sum = 0;
+    string ret;
+    while(a.size() || b.size() || sum){
+        if(a.size()){
+            sum += a.back() - '0';
+            a.pop_back();
+        }
+        if(b.size()){
+            sum += b.back() - '0';
+            b.pop_back();
+        }
+        ret += (sum % 10) + '0';
+        sum /= 10;
+    }
+    reverse(ret.begin(), ret.end());
+    return ret;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> a >> b;
-    if (a.length() > b.length()) {
-        swap(a, b);
-    }
-    for(int i = 0; i < a.length(); i++) {
-        int x = a[a.length() - 1 - i] - '0', y = b[b.length() - 1 - i] - '0';
-        int sum = x + y + z;
-        z = sum / 10;
-        v.push_back(sum % 10);
-    }
-    for (int i = a.length(); i < b.length(); i++) {
-        int y = b[b.length() - 1 - i] - '0';
-        int sum = y + z;
-        z = sum / 10;
-        v.push_back(sum % 10);
-    }
-    if (z > 0) v.push_back(z);
-    reverse(v.begin(), v.end());
-    for(int i = 0; i < v.size(); i++) {
-        cout << v[i];
-    }
+    cout << string_add(a, b) << '\n';
+    return 0;
 }
