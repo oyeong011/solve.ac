0fd294610bfb984ca27551ee7857bbac8f3ab2d0
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-11-14 17:55:45 +0900

    [Gold IV] Title: 문자열 폭발, Time: 80 ms, Memory: 7900 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s, t, ret;
+int main() {
+    cin >> s >> t;
+    for (auto a : s) {
+        ret += a;
+        if (ret.size() >= t.size() && ret.substr(ret.size() - t.size(), t.size()) == t) {
+            ret.erase(ret.end() - t.size(), ret.end());
+        }
+    }
+
+    if (!ret.size()) {
+        cout << "FRULA";
+        return 0;
+    }
+    cout << ret << "\n";
+}
