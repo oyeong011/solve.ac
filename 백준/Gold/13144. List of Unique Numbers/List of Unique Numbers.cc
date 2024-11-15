dff175003708ef509ab301abdca4cfd64dfe028a
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-11-15 15:27:47 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 12 ms, Memory: 3772 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
@@ -3,6 +3,7 @@ using namespace std;
 typedef long long ll;
 ll n, ret, a[100004], visited[100004], s, e;
 int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     cin >> n;
     for(int i = 0; i < n; i++)cin >> a[i];
     
