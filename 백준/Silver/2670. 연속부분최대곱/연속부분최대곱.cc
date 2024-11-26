b0579ed0824ddc2a80d79ed8c607edaca04d2815
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-11-26 18:08:12 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 0 ms, Memory: 2368 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -1,30 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n;
-double ret = -1, a;
-vector<double> v;
-void solve(int idx) {
-    if (v[idx] <= 1) {
-        ret = max(ret, v[idx]);
-        return;
-    }
-    double sum = 1;
-    for (int i = idx; i < n; i++) {
-        sum *= v[i];
-        if (sum <= 1) return;
-        ret = max(ret, sum);
-    }
-}
+double psum[10001], a[10001], ret, b;
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);  // 추가
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> a; v.push_back(a);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    double b = a[0];
+    ret = b;
+    for (int i = 1; i < n; i++) {
+        if (a[i] > b * a[i]) b = a[i];
+        else b *= a[i];
+        ret = max(ret, b);
     }
-
-    for (int i = 0; i < n; i++) {
-        solve(i);
-    }
-    cout << fixed << setprecision(3) << ret << '\n';
-
+    cout << fixed << setprecision(3) << ret << "\n";
 }
