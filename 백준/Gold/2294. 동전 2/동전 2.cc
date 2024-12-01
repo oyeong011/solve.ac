0776d09c0dedcf26c8cdfcafdcdfae4e877f1e44
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-12-01 17:05:55 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2248 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -2,22 +2,20 @@
 using namespace std;
 const int INF = 987654321;
 int n, k, ret, a, dp[10004];
-vector<int> v;
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> k;
-    v.resize(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
     fill(dp, dp + 10004, INF);
-
     dp[0] = 0;
-    for (auto coin : v) {
-        for (int i = coin; i <= k; i++) {
-            if (dp[i - coin] != INF) dp[i] = min(dp[i], dp[i - coin] + 1);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        for (int j = a; j <= k; j++) {
+            dp[j] = min(dp[j], dp[j - a] + 1);
         }
     }
+
+
     if (dp[k] != INF)cout << dp[k] << '\n';
     else cout << -1 << '\n';
 }
