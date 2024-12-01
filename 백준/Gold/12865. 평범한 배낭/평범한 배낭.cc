048527564b55ce6298f0de3fc6574c1465a98d5c
oyeong <53222742+oyeong011@users.noreply.github.com>
2024-12-01 23:27:21 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 5960 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, k, w, v, dp[1000004], mx = 0;
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> w >> v;
+        for (int j = k; j >= w; j--) {
+            dp[j] = max(dp[j], dp[j - w] + v);
+        }
+    }
+    cout << dp[k] << '\n';
+}
