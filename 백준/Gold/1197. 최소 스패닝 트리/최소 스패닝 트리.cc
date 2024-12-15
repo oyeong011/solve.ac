659c510e9b8b25ecc65f187168c8cb999237b808
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2024-12-15 19:17:22 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4944 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int V, E, parent[100001], ans;
+typedef pair<int, pair<int, int>> T;
+vector<T> v;
+int findParent(int x){
+    if(parent[x] == x) return x;
+    return parent[x] = findParent(parent[x]);
+}
+void unionParent(int a, int b){
+    a = findParent(a);
+    b = findParent(b);
+    parent[b] = a;
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> V >> E;
+    vector<T> v;
+    for(int i = 0; i < E; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        v.push_back({c, {a, b}});
+    }
+    sort(v.begin(), v.end());
+    for(int i = 1; i <= V; i++){
+        parent[i] = i;
+    }
+    
+    int cnt = 0;
+    for(int i = 0; i < v.size(); i++){
+        T curEdge = v[i];
+        int cost = curEdge.first;
+        int now = curEdge.second.first;
+        int next = curEdge.second.second;
+        
+        if(findParent(now) == findParent(next)) continue;
+        unionParent(now, next);
+        ans += cost;
+        if(++cnt == V - 1) break;
+    }
+    
+    cout << ans;
+}
