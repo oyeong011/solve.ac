5fa89d63367cbd9d425fcd260e016a0a957ccf4e
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-02-06 12:19:37 +0900

    [Gold II] Title: 보석 도둑, Time: 180 ms, Memory: 15432 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,30 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, k;
 typedef long long ll;
-ll ret;
-vector<pair<ll,ll>> v;
-vector<ll> bag;
-int main() {
+ll n, k, ret, temp1, temp2;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
     cin >> n >> k;
-    for(int i = 0; i < n; i++) {
-        int a, b; cin >> a >> b;
-        v.push_back(make_pair(a, b));
+    
+    vector<pair<ll, ll>> v(n);
+    vector<ll> vv(k);
+    
+    for(int i = 0; i < n; i++){
+        cin >> v[i].first >> v[i].second;
     }
-    for (int i = 0 ; i < k; i++) {
-        int a; cin >> a;
-        bag.push_back(a);
-    }
+    for(int i = 0; i < k; i++) cin >> vv[i];
+    sort(vv.begin(), vv.end());
     sort(v.begin(), v.end());
-    sort(bag.begin(), bag.end());
     priority_queue<ll> pq;
-
+    
     int j = 0;
-    for (auto c : bag) {
-        while (j < n && v[j].first <= c) pq.push(v[j++].second);
-        if (!pq.empty()) {
+    for(int i = 0; i < k; i++){
+        while(j < n && v[j].first <= vv[i]){
+            pq.push(v[j++].second);
+        }
+        if(pq.size()){
             ret += pq.top(); pq.pop();
         }
     }
+    
     cout << ret << "\n";
 }
