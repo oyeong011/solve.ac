b3e735bb622245fd7cf4c455737cca51ac5f7912
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-08-19 14:47:20 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,14 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-string str, str1;
+
 int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    cin >> str;
-    str1 = str;
-    reverse(str.begin(), str.end());
-    if(str == str1){
-        cout << '1' << '\n';
-        return 0;
-    }
-    cout << '0' << '\n';
+    string s, tmp;
+    cin >> s;
+    tmp = s;
+    reverse(tmp.begin(), tmp.end());
+    if(tmp == s) cout << 1 <<"\n";
+    else cout << 0 << "\n";
 }
