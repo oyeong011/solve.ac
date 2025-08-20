6189b97ceb81347f33c3bcb1ba98eaf9520304e2
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-08-20 18:09:10 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,27 +1,34 @@
-#include<bits/stdc++.h> 
-using namespace std; 
-string s, ret; 
-int cnt[200], flag; 
-char mid;
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> s;
-	for(char a : s)cnt[a]++;
-	for(int i = 'Z'; i >= 'A'; i--){
-		if(cnt[i]){
-			if(cnt[i] & 1){
-				mid = char(i);flag++;
-				cnt[i]--;
-			}
-			if(flag == 2)break;
-			for(int j = 0; j < cnt[i]; j += 2){
-				ret = char(i) + ret; 
-				ret += char(i);
-			}
-		}
-	}
-	if(mid)ret.insert(ret.begin() + ret.size() / 2, mid);
-	if(flag == 2)cout << "I'm Sorry Hansoo\n";
-	else cout << ret << "\n"; 
+#include<bits/stdc++.h>
+using namespace std;
+
+int flag = 0;
+string s, sol;
+int ret[200];
+char mid = '\0';
+
+int main(){
+    cin >> s;
+    for(auto c : s){
+        ret[c]++;
+    }
+    
+    for(int i = 'Z'; i >= 'A'; i--){
+        if(ret[i] & 1){
+            flag++;
+            mid = char(i);
+            ret[i]--;
+        }
+        if(flag == 2)break;
+        
+        for(int j=0;j<ret[i];j+=2){
+            sol += char(i);
+            sol = char(i) + sol;
+        }
+    }
+    
+    if(mid)sol.insert(sol.begin() + sol.size() / 2, mid);
+    if(flag > 1) cout << "I'm Sorry Hansoo";
+    else cout << sol;
+    return 0;
+    
 }
