098cf4432dc28d8e243b67cc73b8a19f7a45bafb
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-08-21 17:10:38 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2296 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,42 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define y1 aaaa
-int m, n, k, x, y, nx, ny, a[104][104], visited[104][104], x1, x2, y1, y2;
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-vector<int> ret;
-int dfs(int y, int x){
-    visited[y][x] = 1;
-    int ret = 1;
-    for(int i = 0; i < 4; i++){
-        nx = x + dx[i];
-        ny = y + dy[i];
-        if(nx < 0 || nx >= n || ny < 0 || ny >= m || visited[ny][nx] == 1)continue;
-        if(a[ny][nx] == 1)continue;
-        ret += dfs(ny, nx);
+
+int m, n, k;
+int a[101][101];
+int vis[101][101];
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+int bfs(int sy, int sx){
+    queue<pair<int, int>> q;
+    q.push({sy, sx});
+    vis[sy][sx] = 1;
+    int area = 1;
+    
+    while(!q.empty()){
+        auto [y, x]=q.front();
+        q.pop();
+        for(int dir = 0; dir<4;dir++){
+            int ny = y +dy[dir];
+            int nx = x + dx[dir];
+            if(ny < 0||nx < 0 || ny >= m || nx >= n) continue;
+            if(vis[ny][nx] || a[ny][nx]) continue;
+            vis[ny][nx] = 1;
+            q.push({ny, nx});
+            area++;
+        }
     }
-    return ret;
+    return area;
 }
+
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     cin >> m >> n >> k;
-    for(int i = 0; i < k; i++){
+    for(int t=0;t<k;t++){
+        int x1, x2, y1, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        for(int x = x1; x < x2; x++){
-            for(int y = y1; y < y2; y++){
-                a[y][x] = 1;
+        for(int y = y1; y < y2;y++){
+            for(int x = x1; x < x2; x++){
+                a[y][x]=1;
             }
         }
     }
     
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if(a[i][j] != 1 && visited[i][j] == 0){
-                ret.push_back(dfs(i, j));
-            }
+    vector<int> areas;
+    for(int y = 0;y < m;y++){
+        for(int x = 0; x < n; x++){
+            if(a[y][x] || vis[y][x]) continue;
+            areas.push_back(bfs(y, x));
         }
     }
-    sort(ret.begin(), ret.end());
-    cout << ret.size() << "\n";
-    for(int a : ret) cout << a << " ";
+    
+    sort(areas.begin(), areas.end());
+    
+    cout << areas.size() << '\n';
+    for(size_t i = 0; i < areas.size(); i++){
+        cout << areas[i];
+        if(i +1 < areas.size()) cout << ' ';
+    }
+    cout <<'\n';
     return 0;
 }
