404727468e5aadd2cb5a2ec460b38fa547893cd2
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-08-21 23:42:53 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,21 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long int ll;     
+typedef long long int ll;
 int n;
 string s;
-char a[104][104];
-string solve(int y, int x, int size){
-    if(size == 1)return string(1, a[y][x]);
+char a[101][101];
+
+string dfs(int y, int x, int size){
+    if(size == 1) return string(1, a[y][x]);
     char b = a[y][x];
     string ret = "";
-    for(int i = y; i < y + size; i++){
-        for(int j = x; j < x + size; j++){
+    
+    for(int i=y;i<y+size; i++){
+        for(int j=x;j<x+size;j++){
             if(b != a[i][j]){
                 ret += '(';
-                ret += solve(y, x, size / 2);
-                ret += solve(y, x + size/2 , size / 2);
-                ret += solve(y + size/2, x, size / 2);
-                ret += solve(y + size/2, x + size/2 , size / 2);
+                ret += dfs(y, x, size / 2);
+                ret += dfs(y, x + size / 2, size / 2);
+                ret += dfs(y + size / 2, x, size/2);
+                ret += dfs(y + size / 2, x + size /2, size /2);
                 ret += ')';
                 return ret;
             }
@@ -23,15 +25,17 @@ string solve(int y, int x, int size){
     }
     return string(1, a[y][x]);
 }
+
 int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> n;
-    for(int i = 0; i < n; i++){
+    for(int i=0;i<n;i++){
         cin >> s;
-        for(int j = 0; j < n; j++){
+        for(int j=0;j<n;j++){
             a[i][j] = s[j];
         }
     }
-    cout << solve(0,0,n)<< "\n";
+    cout << dfs(0, 0, n) << "\n";
     return 0;
 }
