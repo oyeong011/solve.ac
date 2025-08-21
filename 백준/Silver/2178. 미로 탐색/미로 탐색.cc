930964632f2e3966ac9ce3f840b86c860f609399
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-08-21 09:55:15 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,33 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int max_n = 104;
-int n, m, x, y, ny, nx, a[max_n][max_n], visited[max_n][max_n];
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
+int n, m;
+int a[101][101];
+string s;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+
 int main(){
     cin >> n >> m;
-    for(int i = 0; i < n; i++){
-        string row;
-        cin >> row;
-        for(int j = 0; j < m;j++){
-            a[i][j] = row[j] - '0';
+    for(int i=0;i<n;i++){
+        cin >> s;
+        for(int j=0;j<m;j++)
+            a[i][j] = s[j] - '0';
+    }
+    queue<pair<int,int>> q;
+    q.push({0, 0});
+    a[0][0] = 1;
+    while(!q.empty()){
+        auto [x,y] = q.front(); q.pop();
+        for(int i=0;i<4;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;
+            if(a[nx][ny] != 1) continue;
+            a[nx][ny] = a[x][y] + 1;
+            q.push({nx, ny});
         }
     }
-    
-    queue<pair<int, int>> q;
-    visited[0][0] = 1;
-    q.push({0,0});
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop();
-        for(int i = 0; i < 4; i++){
-            ny = y + dy[i];
-            nx = x + dx[i];
-            if(ny < 0 || nx < 0 || nx >= m || ny >= n || a[ny][nx] == 0)continue;
-            if(visited[ny][nx])continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
-        }
-    }
-    cout << visited[n-1][m-1];
-    
+    cout << a[n-1][m-1];
+    return 0;
 }
