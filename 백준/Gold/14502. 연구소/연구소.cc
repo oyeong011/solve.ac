a23b5b0d670ae9cef1e7d9443cca3553fb5c4fbf
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-08-28 13:59:51 +0900

    [Gold IV] Title: 연구소, Time: 32 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,56 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, x, y, ret;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-vector<pair<int, int>> virusList, wallList;
-int a[10][10], visited[10][10];
+int n, m, ret;
+int a[10][10], vis[10][10];
+vector<pair<int, int>> vList, wList;
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
 void dfs(int y, int x){
-    for(int i = 0; i < 4; i++){
+    for(int i = 0;i<4;i++){
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if(ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || a[ny][nx] == 1)continue;
-        visited[ny][nx] = 1;
+        if(ny < 0 || nx < 0 || ny >= n || nx >= m || vis[ny][nx] || a[ny][nx] == 1)continue;
+        vis[ny][nx] = 1;
         dfs(ny, nx);
     }
-    return;
 }
-int solve(){
-    fill(&visited[0][0], &visited[0][0] + 10 * 10, 0);
-    for(auto b : virusList){
-        visited[b.first][b.second] = 1;
+int sol(){
+    fill(&vis[0][0], &vis[0][0] + 100, 0);
+    for(pair<int, int> b : vList){
+        vis[b.first][b.second] = 1;
         dfs(b.first, b.second);
     }
-    
     int cnt = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(a[i][j] == 0 && !visited[i][j])cnt++;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(a[i][j] == 0 && vis[i][j] == 0)cnt++;
         }
     }
     return cnt;
 }
+
 int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> n >> m;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
             cin >> a[i][j];
-            if(a[i][j] == 2)virusList.push_back({i, j});
-            if(a[i][j] == 0)wallList.push_back({i, j});
+            if(a[i][j] == 2) vList.push_back({i, j});
+            if(a[i][j] == 0) wList.push_back({i,j});
         }
     }
     
-    for(int i = 0; i < wallList.size(); i++){
-        for(int j = 0; j < i; j++){
-            for(int k = 0; k < j; k++){
-                a[wallList[i].first][wallList[i].second] = 1;
-                a[wallList[j].first][wallList[j].second] = 1;
-                a[wallList[k].first][wallList[k].second] = 1;
-                ret = max(ret, solve());
-                a[wallList[i].first][wallList[i].second] = 0;
-                a[wallList[j].first][wallList[j].second] = 0;
-                a[wallList[k].first][wallList[k].second] = 0;
+    for(int i=0;i<wList.size();i++){
+        for(int j=0;j<i;j++){
+            for(int k=0;k<j;k++){
+                a[wList[i].first][wList[i].second] = 1;
+                a[wList[j].first][wList[j].second] = 1;
+                a[wList[k].first][wList[k].second] = 1;
+                ret = max(ret, sol());
+                a[wList[i].first][wList[i].second] = 0;
+                a[wList[j].first][wList[j].second] = 0;
+                a[wList[k].first][wList[k].second] = 0;
             }
         }
     }
