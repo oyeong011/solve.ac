54ee9b14eba1e363a78bde44ea88f6b3a163bca2
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-09-16 17:36:45 +0900

    [Gold IV] Title: 알파벳, Time: 256 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,33 +1,43 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int R, C, ret, ny, nx, visited[30];
-char a[21][21];
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = { 0, 1, 0, -1}; 
-void go(int y, int x, int cnt){
+int dy[] = {1, 0, -1, 0};
+int dx[] = {0, -1, 0, 1};
+int R, C, vis[26], ret;
+char a[24][24];
+
+void dfs(int y, int x, int cnt) {
     ret = max(ret, cnt);
-    for(int i = 0; i < 4; i++){
-        ny = y + dy[i], nx = x + dx[i];
-        if(ny < 0 || ny >= R || nx < 0 || nx >= C) continue;
-        int next = (int)(a[ny][nx] - 'A');
+    
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
+        int next = a[ny][nx] - 'A';
+        if (vis[next]) continue;
         
-        if(visited[next] == 0){
-            visited[next] = 1; 
-            go(ny, nx, cnt + 1);
-            visited[next] = 0;  
-        } 
+        vis[next] = 1;
+        dfs(ny, nx, cnt + 1);
+        vis[next] = 0;
     }
-    return;
 }
-int main(){
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
     cin >> R >> C;
-    for(int i = 0; i < R; i++){
-        for(int j = 0; j < C; j++){
-            cin >> a[i][j];
+    for (int i = 0; i < R; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < C; j++) {
+            a[i][j] = s[j];
         }
     }
-    visited[(int)a[0][0] - 'A'] = 1;
-    go(0, 0, 1);
-    cout << ret << '\n';
+    
+    memset(vis, 0, sizeof(vis));
+    vis[a[0][0] - 'A'] = 1;  // 시작 위치의 알파벳 방문 체크
+    dfs(0, 0, 1);
+    
+    cout << ret << "\n";
     return 0;
 }
