a333536c51171fccce5db6b50679888b67ee20ae
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2025-09-24 17:25:08 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -1,35 +1,35 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+int k, mp[1400];
 vector<int> ret[14];
-int k, mp[1005];
-
-void go(int s, int e, int level) {
-    if (s > e) return;
-    if (s == e) {
-        ret[level].push_back(mp[s]);
+void go(int s, int e, int dep){
+    if(s > e)return;
+    if(s == e){
+        ret[dep].push_back(mp[s]);
         return;
     }
-    int mid = (s + e) / 2;
-    ret[level].push_back(mp[mid]);
-    go(s, mid - 1, level + 1);
-    go(mid + 1, e, level + 1);
-    return;
+    int mid = (s+e)/2;
+    ret[dep].push_back(mp[mid]);
+    go(s, mid - 1, dep + 1);
+    go(mid + 1, e, dep + 1);
+    return ;
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
     cin >> k;
-    int _end = (int)pow(2, k) - 1;
-    for (int i = 0; i < _end; i++) {
+    int _end = (int)pow(2, k) -1 ;
+    for(int i=0;i<_end;i++){
         cin >> mp[i];
     }
-    go(0, _end, 1);
-    for (int i = 1; i <= k; i++) {
-        for (int j : ret[i]) {
+    
+    go(0, _end, 0);
+    for(int i=0;i < k; i++){
+        for(int j : ret[i]){
             cout << j << ' ';
         }
-        cout << '\n';
+        cout << "\n";
     }
-
     return 0;
 }
