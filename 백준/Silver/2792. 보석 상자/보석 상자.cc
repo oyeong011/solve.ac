7715f35505291a89cb5ba0e6633afe504d454314
oyeong011 <53222742+oyeong011@users.noreply.github.com>
2026-01-20 23:19:01 +1100

    [Silver I] Title: 보석 상자, Time: 60 ms, Memory: 5324 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.cc
+++ 백준/Silver/2792. 보석 상자/보석 상자.cc
@@ -2,25 +2,29 @@
 using namespace std;
 int n, m, mid, ret = 1e9;
 vector<int> v;
+
 bool go(int val) {
     int cnt = 0;
-    for (int a : v) {
+    for (int a: v) {
         cnt += a / val;
         if (a % val)cnt++;
     }
     return cnt <= n;
 }
+
 int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     cin >> n >> m;
     int left = 1, right = 0;
     for (int i = 1; i <= m; i++) {
-        int a; cin >> a;
+        int a;
+        cin >> a;
         v.push_back(a);
         right = max(right, a);
     }
-
     while (left <= right) {
         mid = (left + right) / 2;
         if (go(mid)) {
